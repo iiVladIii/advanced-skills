5c32958951bff097295aab1d3b53c5432133e7c7
iiVladii <vladislav2407@icloud.com>
2023-01-29 17:51:39 +0300

    webpack env

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,30 @@
 import webpack from 'webpack'
 import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
+import dotenv from 'dotenv'
 
-const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
+export default (env: BuildEnv): webpack.Configuration => {
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+
+    dotenv.config()
+
+    const mode = env.mode || 'development'
+    const isDev = mode === 'development'
+    const PORT = env.port || Number(process.env.PORT) || 3000
+
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode: mode,
+        paths: paths,
+        isDev: isDev,
+        port: PORT
+    })
+
+    return config
 }
-
-const mode = 'development'
-const isDev = mode === 'development'
-
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
-    paths: paths,
-    isDev: isDev,
-})
-
-export default config
