2fe3dfd1c6b62dbaf5dcff5e67f23f774fe39415
iiVladii <vladislav2407@icloud.com>
2023-01-29 22:00:29 +0300

    router dom code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            smt text
-            <Counter/>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+
+            <Suspense fallback={<div>loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
