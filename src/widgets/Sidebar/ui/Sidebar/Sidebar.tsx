b41eee848640d81db5a6f0725e54980ecc6ede98
iiVladii <vladislav2407@icloud.com>
2023-02-07 20:57:31 +0300

    sidebar, i18n, langSwitcher

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import cls from './Sidebar.module.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import React, {useState} from "react";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher";
+
+interface SidebarProps {
+    className?: string
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+
+    return (
+        <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+            <div className={cls.switchers}>
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
+            </div>
+            <button onClick={onToggle}>toggle</button>
+        </div>
+    );
+};
