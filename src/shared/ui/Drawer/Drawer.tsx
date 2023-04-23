ccfe9c55566cbacfdb88eef13dc98efb8259435b
iiVladii <vladislav2407@icloud.com>
2023-04-23 21:43:37 +0300

    upgrade drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,5 +1,6 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { memo, ReactNode } from 'react';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Portal } from '../Portal/Portal';
 import cls from './Drawer.module.scss';
 import { Overlay } from '../Overlay/Overlay';
@@ -9,6 +10,7 @@ interface DrawerProps {
     children: ReactNode;
     isOpen?: boolean;
     onClose?: ()=>void;
+    lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -17,17 +19,32 @@ export const Drawer = memo((props: DrawerProps) => {
         children,
         onClose,
         isOpen,
+        lazy,
     } = props;
 
     // const { theme } = useTheme();
 
+    const { isClosing, isMounted, close } = useModal(
+        {
+            animationDelay: 300,
+            onClose,
+            isOpen,
+        },
+    );
+
     const mods: Mods = {
         [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
+
     return (
         <Portal>
             <div className={classNames(cls.Drawer, mods, [className])}>
-                <Overlay onClick={onClose} />
+                <Overlay onClick={close} />
                 <div
                     className={cls.content}
                 >
