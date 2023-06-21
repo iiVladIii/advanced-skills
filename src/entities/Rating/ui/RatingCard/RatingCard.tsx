b62abf58c835b486b0fc811a5527b21902bc976f
iiVladii <vladislav2407@icloud.com>
2023-06-21 16:06:36 +0300

    add star rating, add Rating entity and rating card

---
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -0,0 +1,122 @@
+import { memo, useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './RatingCard.module.scss';
+import { Card } from '@/shared/ui/Card/Card';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text/Text';
+import { StarRating } from '@/shared/ui/StarRating/StarRating';
+import { Modal } from '@/shared/ui/Modal/Modal';
+import { Input } from '@/shared/ui/Input/Input';
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button/Button';
+import { useDeviceDetect } from '@/shared/lib/hooks/useDeviceDetect/useDeviceDetect';
+import { Drawer } from '@/shared/ui/Drawer/Drawer';
+
+interface RatingCardProps {
+    className?: string;
+    title?: string;
+    feedbackTitle?: string;
+    hasFeedback?: boolean;
+    onCancel?: (starsCount: number) => void;
+    onAccept?: (starsCount: number, feedback?: string) => void;
+}
+
+export const RatingCard = memo((props: RatingCardProps) => {
+    const {
+        className,
+        title,
+        feedbackTitle,
+        onCancel,
+        onAccept,
+        hasFeedback,
+    } = props;
+    const { t } = useTranslation();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [starsCount, setStarsCount] = useState(0);
+    const [feedback, setFeedback] = useState('');
+
+    const onSelectStars = useCallback((selectedStarsCount: number) => {
+        setStarsCount(selectedStarsCount);
+        if (hasFeedback) {
+            setIsModalOpen(true);
+        } else {
+            onAccept?.(selectedStarsCount);
+        }
+    }, [hasFeedback, onAccept]);
+
+    const acceptHandler = useCallback(() => {
+        setIsModalOpen(false);
+        onAccept?.(starsCount, feedback);
+    }, [feedback, onAccept, starsCount]);
+
+    const cancelHandler = useCallback(() => {
+        setIsModalOpen(false);
+        onCancel?.(starsCount);
+    }, [onCancel, starsCount]);
+
+    const modalContent = (
+        <>
+            <Text title={feedbackTitle} />
+            <Input value={feedback} onChange={setFeedback} placeholder={t('Ваш отзыв')} />
+        </>
+    );
+
+    const isMobile = useDeviceDetect();
+
+    return (
+        <Card className={classNames(cls.RatingCard, {}, [className])}>
+            <VStack gap="8" align="center">
+                <Text title={title} />
+                <StarRating
+                    size={40}
+                    onSelect={onSelectStars}
+                />
+            </VStack>
+            {!isMobile
+                ? (
+                    <Modal
+                        isOpen={isModalOpen}
+                        lazy
+                    >
+                        <VStack max gap="32">
+                            {modalContent}
+                            <HStack gap="16" justify="end">
+                                <Button
+                                    onClick={cancelHandler}
+                                    theme={ButtonTheme.OUTLINE_RED}
+                                >
+                                    {t('Закрыть')}
+                                </Button>
+                                <Button
+                                    onClick={acceptHandler}
+                                    theme={ButtonTheme.OUTLINE}
+                                >
+                                    {t('Отправить')}
+                                </Button>
+                            </HStack>
+                        </VStack>
+                    </Modal>
+                )
+                : (
+                    <Drawer
+                        lazy
+                        isOpen={isModalOpen}
+                        onClose={cancelHandler}
+                    >
+                        <VStack gap="32">
+                            {modalContent}
+                            <Button
+                                fullWidth
+                                size={ButtonSize.L}
+                                onClick={acceptHandler}
+                                // theme={ButtonTheme.OUTLINE}
+                            >
+                                {t('Отправить')}
+                            </Button>
+                        </VStack>
+                    </Drawer>
+                )}
+        </Card>
+    );
+});
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
+import { StarRating } from '@/shared/ui/StarRating/StarRating';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
         <Page>
             {t('Главная страница')}
+            <StarRating size={50} />
         </Page>
     );
 };
