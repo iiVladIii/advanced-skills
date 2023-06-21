b0487e254afd416351cc1c1b76851ed171b7cf71
iiVladii <vladislav2407@icloud.com>
2023-06-21 16:07:42 +0300

    add star rating, add Rating entity and rating card

--- src/entities/Rating/ui/RatingCard/RatingCard.tsx
+++ src/entities/Rating/ui/RatingCard/RatingCard.tsx
@@ -1,7 +1,6 @@
 import { memo, useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './RatingCard.module.scss';
 import { Card } from '@/shared/ui/Card/Card';
 import { HStack, VStack } from '@/shared/ui/Stack';
 import { Text } from '@/shared/ui/Text/Text';
@@ -65,7 +64,7 @@ export const RatingCard = memo((props: RatingCardProps) => {
     const isMobile = useDeviceDetect();
 
     return (
-        <Card className={classNames(cls.RatingCard, {}, [className])}>
+        <Card className={classNames('', {}, [className])}>
             <VStack gap="8" align="center">
                 <Text title={title} />
                 <StarRating
