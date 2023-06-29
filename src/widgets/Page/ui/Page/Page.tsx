581a412534aa7918264e8b896d5d1f1802c1448b
iiVladii <vladislav2407@icloud.com>
2023-06-29 16:33:00 +0300

    move UI from widgets to features

--- src/widgets/Page/ui/Page/Page.tsx
+++ src/widgets/Page/ui/Page/Page.tsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useInfiniteScroll } from '@/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { getUIScrollByPath, uiActions } from '../../../UI';
+import { getUIScrollByPath, uiActions } from '@/features/UI';
 import { useInitialEffect } from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { useThrottle } from '@/shared/lib/hooks/useThrottle/useThrottle';
