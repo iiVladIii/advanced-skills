4c46e30bf9307e4537d85c80556b53eb4105d928
iiVladii <vladislav2407@icloud.com>
2023-09-07 21:05:41 +0300

    get api url function

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,17 @@ import webpack from 'webpack';
 import path from 'path';
 import dotenv from 'dotenv';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+    if (apiUrl) {
+        return apiUrl;
+    }
+    if (mode === 'production') {
+        return '/api';
+    }
+    return 'http://localhost:8080';
+}
 
 export default (env: BuildEnv): webpack.Configuration => {
     const paths: BuildPaths = {
@@ -19,9 +29,7 @@ export default (env: BuildEnv): webpack.Configuration => {
     const mode = env?.mode || 'development';
     const isDev = mode === 'development';
     const PORT = env?.port || Number(process.env.PORT) || 3000;
-    const apiUrl = isDev
-        ? 'http://localhost:8080'
-        : process.env.API_URL || env?.apiUrl;
+    const apiUrl = getApiUrl(mode, process.env.API_URL || env?.apiUrl);
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
